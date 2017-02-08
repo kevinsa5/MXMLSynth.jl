0be254fd36c793e8677767d34f27a4db512bf2fe
Kevin Anderson <kevinanderso@gmail.com>
2017-02-07 23:21:09 -0800

    push to github

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # MXMLSynth
+
+A simple audio synthesizer designed to be used with the MusicXML library.  Very much still a work in progress.
+
+Example usage:
+
+    using MusicXML
+    using MXMLSynth
+    using PortAudio
+    using SampledSignals
+
+    cd(joinpath(homedir(), "MusicXML", "mxml_tests"))
+
+    @time begin
+      song = MusicXML.parseMXMLFile("fur_elise.xml")
+      sb = MXMLSynth.synthesizeTrack(song)
+    end
+
+    sbs = SampleBufSource(sb)
+    out = PortAudioStream("default")
+    write(out, sbs)
+    #MXMLSynth.saveTrack(joinpath(homedir(), "furry-lease2.ogg"), sb)
--- src/MXMLSynth.jl
+++ src/MXMLSynth.jl
@@ -1,5 +1,54 @@
 module MXMLSynth
 
-# package code goes here
+using LibSndFile
+using PortAudio
+using SampledSignals
+using MusicXML
 
-end # module
+no_smoother(L,t) = 1
+quadratic_smoother(L,t) = t^2 / (L/10000 + t^2)
+cubic_smoother(L,t) = t^3 / (L/100000 + t^3)
+
+function createSmoother(asymm_smoother, L)
+  return t -> asymm_smoother(L,t) * asymm_smoother(L,L-t)
+end
+
+function calcFrequency(pitch::MusicXML.Pitch)
+  # calculate the number of half-steps from A440 = A4
+  id = pitch.octave * 12 + Int(pitch.step) + pitch.alter
+  nhalfsteps = id - 58
+  return 440 * 2 ^ (nhalfsteps/12)
+end
+
+function saveTrack(fpath::String, sb::SampledSignals.SampleBuf)
+  LibSndFile.save(fpath, sb)
+end
+
+# Returns a SampledSignals.SampleBuf
+function synthesizeTrack(tree::MusicXML.MXMLTree,
+                         sample_rate = 4.41e4,
+                         bpm = 120*30,
+                         smoothing_function = quadratic_smoother)
+  buffer = Float64[]
+  smoother = createSmoother(smoothing_function, 10/sample_rate)
+  samples_per_beat = round(sample_rate * 60 / bpm)
+  for part in tree.parts
+    for measure in part.measures
+      for note in measure.notes
+        num_samples = round(Int, samples_per_beat * note.duration / 4);
+        if note.isrest
+          append!(buffer, zeros(Float64, num_samples))
+        else
+          note_length = num_samples / sample_rate
+          t = linspace(0, note_length, num_samples)
+          freq = calcFrequency(note.pitch)
+          y = 1.0 * sin.(2*pi*freq*t) .* smoother.(t)
+          append!(buffer, y)
+        end
+      end
+    end
+  end
+  return SampleBuf(buffer, sample_rate)
+end
+
+end
